096b2200a1e5635267d5225da62953c87e49763b
Maddie <maddiemcampbell@gmail.com>
2019-09-12 13:23:23 -0700

    console log and displays total due as whole number

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// Write your JavaScript here
+var amountDue = document.getElementById('amount-due').value;
+var amountReceived = document.getElementById('amount-received').value;
+
+function calculateAmount(amountDue, amountReceived){
+    var amountDue = document.getElementById('amount-due').value;
+    var amountReceived = document.getElementById('amount-received').value;
+    return amountDue - amountReceived;
+}
+
+
+function handleClickEvent(){
+    var displayDue = calculateAmount(amountDue, amountReceived);
+    console.log(displayDue);
+    document.getElementById("total-due").innerHTML= displayDue;
+}
+
