11a6ad5afde81a01e6db7fda874109a7decc2119
Maddie <maddiemcampbell@gmail.com>
2019-09-13 10:43:42 -0700

    calculator working

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-var amountDue = document.getElementById('amount-due').value;
-var amountReceived = document.getElementById('amount-received').value;
-
-function calculateAmount(amountDue, amountReceived){
-    var amountDue = document.getElementById('amount-due').value;
-    var amountReceived = document.getElementById('amount-received').value;
-    return amountDue - amountReceived;
-}
-
-
 function handleClickEvent(){
-    var displayDue = calculateAmount(amountDue, amountReceived);
-    console.log(displayDue);
-    document.getElementById("total-due").innerHTML= displayDue;
+    var received = document.getElementById("amount-received").value;
+    var due = document.getElementById("amount-due").value;
+    var change = received - due;
+
+
+    dollars = Math.floor(change/1);
+    change = (change % 1).toFixed(2);
+
+    quarters = Math.floor(change/.25);
+    change = (change % .25).toFixed(2);
+
+    dimes = Math.floor(change/.10);
+    change = (change % .10).toFixed(2);
+   
+    nickels = Math.floor(change/.05);
+    change = (change % .05).toFixed(2);
+    
+    pennies = Math.floor(change/.01);
+    change = (change % .01).toFixed(2);
+
+
+    document.getElementById("dollars-output").innerText = dollars;
+    document.getElementById("quarters-output").innerText = quarters;
+    document.getElementById("dimes-output").innerText = dimes;
+    document.getElementById("nickels-output").innerText = nickels;
+    document.getElementById("pennies-output").innerText = pennies;
 }
 
